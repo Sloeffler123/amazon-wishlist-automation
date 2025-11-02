8ebaf23395a5e752685e3af1f6e5a999473c4a32
Sam Loeffler <samloeffler123@gmail.com>
2025-11-02 05:22:38 -0600

    main data scap done

--- harvest.py
+++ harvest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import requests
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+import time
 URL = "https://www.amazon.com/hz/wishlist/ls/299PQKIASMWBC/ref=nav_wishlist_lists_1"
 
 chrome_options = webdriver.ChromeOptions()
@@ -10,7 +13,58 @@ driver = webdriver.Chrome(options=chrome_options)
 
 
 driver.get(URL)
+driver.fullscreen_window()
+time.sleep(2)
+
+def clear_empty_elems(list):
+    # making a new list in case I have to use the old one
+    new_list = []
+    for i in list: 
+        if len(i.text) < 1:
+            continue
+        else:
+            new_list.append(i)
+    return new_list        
+
+def scroll_loop(scroll):
+    scroll_origin = ScrollOrigin.from_element(scroll)
+    ActionChains(driver).scroll_from_origin(scroll_origin, 0, 700).perform()
+    time.sleep(3)
+    not_end = True
+    while not_end:
+        ActionChains(driver).scroll_by_amount(0, 700).perform()
+        time.sleep(2)
+        if driver.find_element(By.TAG_NAME, "h5").text == "End of list":
+            not_end = False
 
 
 
+try:
+    button = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[3]/div/div/form/div/div/span/span/button")
+    if button:
+        button.click()
+        driver.fullscreen_window()
+        time.sleep(2)
+except NoSuchElementException:
+    print("Button wasnt there")
 
+
+scroll_to =  driver.find_element(By.XPATH, "/html/body/div[2]/div[1]/div/div/div/div/div/div[2]/div[8]/div/div/ul/li[11]")
+
+ActionChains(driver).scroll_to_element(scroll_to).perform()
+
+scroll_loop(scroll_to)
+
+title_names = driver.find_elements(By.TAG_NAME, "h2")
+
+price_for_books = driver.find_elements(By.CLASS_NAME, "a-price")
+
+new_title_names = clear_empty_elems(title_names)
+
+book_price_dict = {}
+for name, price in zip(new_title_names, price_for_books):
+    if len(name.text) < 1:
+        continue
+    
+    book_price_dict[name.text] = price.text
+print(book_price_dict)
