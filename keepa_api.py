b9574d79ceb53a384c4ffcbdeef8ed2a393bd781
Sam Loeffler <samloeffler123@gmail.com>
2025-11-13 05:50:10 -0600

    added currency converter and get current price

--- keepa_api.py
+++ keepa_api.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import keepa
-import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,20 +29,36 @@ def seperate_ISBN10_ISBN13():
             isbn_10.append(num)        
     return isbn_10, isbn_13
 
+def convert_to_currency(max, min, avg, current):
+    
+    lst = [max, min, avg, current]
+    new_lst = []
+    for i in lst:
+        j = str(i)
+        if len(j) == 4:
+           num = f"{j[0:2]}.{j[2:4]}"
+        else:
+            num = f"{j[0:1]}.{j[1:3]}"
+        new_lst.append(num)        
+        
+    print(new_lst)    
+
 def api_query():
     
     isbn_10, isbn_13 = seperate_ISBN10_ISBN13()
-    test = isbn_10[0]
-    print(test)
-    isbn_10_products = api.query(items=test, history=False, to_datetime=False, out_of_stock_as_nan=False, progress_bar=False, buybox=False, wait=False, offers=None, stock=False, stats=0)
-    
+
+    isbn_10_products = api.query(items=isbn_10[0], history=False, to_datetime=False, out_of_stock_as_nan=False, progress_bar=False, buybox=False, wait=False, offers=None, stock=False, stats=0)
+
+    amazon_current_price = isbn_10_products[0]["stats"]["current"][0]
+
     amazon_max_price = isbn_10_products[0]["stats"]["max"][0][1]
     amazon_max_price_time = isbn_10_products[0]["stats"]["max"][0][0]
-    amazon_max_price_time = (amazon_max_price_time / 60) / 24
-    print(amazon_max_price_time)
+    
     amazon_min_price = isbn_10_products[0]["stats"]["min"][0][1]
     amazon_min_price_time = isbn_10_products[0]["stats"]["min"][0][0]
 
     amazon_avg365_price = isbn_10_products[0]["stats"]["avg365"][0]
+
+    convert_to_currency(amazon_max_price, amazon_min_price, amazon_avg365_price, amazon_current_price)
 
 api_query()
