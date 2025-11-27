29256e7e9d464203d767b2afc9ec91e979175c65
Sam Loeffler <samloeffler123@gmail.com>
2025-11-27 07:14:28 -0600

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -11,22 +11,8 @@ def make_driver():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-def tear_down(webdriver):
-    webdriver.quit()
-
 def open_browser(webdrive, url):
     webdrive.get(url)
     time.sleep(1)
     webdrive.fullscreen_window()
-    time.sleep(2)
-
-def go_back_open_browser(webdriver):
-    time.sleep(0.5)
-    webdriver.back()
-    time.sleep(0.5)
-    webdriver.fullscreen_window()
-
-def full_screen_window(webdriver):
-    time.sleep(0.5)
-    webdriver.fullscreen_window()
-    
+    time.sleep(1)
